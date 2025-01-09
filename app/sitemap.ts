22451ef3708210e36d4d3c2983e50b4f67eac468
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 01:05:14 +0700

    link

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const nameDomain = "https://camnangphaidep.com";
+const nameDomain = "https://www.potenzpillen.shop";
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
